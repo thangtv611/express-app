57b83be12a2405473299e6969ee6bf8e679dadec
Thang Tran <thangtv611@gmail.com>
2023-05-01 22:47:41 +0700

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Xin chao cac ban' });
 });
 
 module.exports = router;
